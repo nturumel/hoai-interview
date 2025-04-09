5f217c168a80553cf81fec6d36ccc25ec2516126
nihar turumella <nihar.turumella@gmail.com>
2025-04-09 02:56:13 -0500

    fix: Build failures

--- lib/db/migrations/0004_low_starfox.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS InvoiceDocument;
-
-CREATE TABLE InvoiceDocument (
-  invoiceId TEXT NOT NULL,
-  documentUrl TEXT NOT NULL,
-  documentName TEXT NOT NULL,
-  PRIMARY KEY (invoiceId, documentUrl),
-  FOREIGN KEY (invoiceId) REFERENCES Invoice(id)
-);
